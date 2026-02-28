531a9f18d616cf3b2bd6db2315e84b2f6aa86319
embenned <benkoprogram@gmail.com>
2026-02-28 09:14:31 +0100

    poczatek dnia 2802

---
+++ 2801-03-2026/pizzeria_django/pizzeria_project/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path, include
+from django.shortcuts import redirect
+
+urlpatterns = [
+    path('', lambda request: redirect('menu/')),
+    path('menu/', include('menu_app.urls')),
+    path('klienci/', include('customers_app.urls')),
+    path('zamowienia/', include('orders_app.urls')),
+]
